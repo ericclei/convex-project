9aab35e799de4c4f0371fd958b5cc3c67b962450
ericlei-cmu <elei@cs.cmu.edu>
2015-12-14 18:00:25 -0500

    error path experiment and matrices fix

--- jacobi_method.m
+++ jacobi_method.m
@@ -5,13 +5,18 @@ function [x,n_iter] = jacobi_method(A,b)
 n = size(A,1);
 assert(size(A,2)==n);
 D = spdiags(diag(A),0,n,n);
+invD = D^-1;
 R = A-D;
-x = D \ b;
+x = invD* b;
 tol = 1e-6;
 n_iter = 0;
 while 1
   n_iter = n_iter+1;
-  xnew = D \ (b - R*x);
+  z = b-R*x;
+  xnew = invD*z;
+  if mod(n_iter,100)==0
+    disp(max(abs(xnew-x)));
+  end
   if max(abs(xnew-x))<=tol
     x = xnew;
     break
--- successive_over_relaxation.m
+++ successive_over_relaxation.m
@@ -1,23 +1,30 @@
-function [x,n_iter] = successive_over_relaxation(A,b,w,verbose)
+function [x,n_iter,err_path] = successive_over_relaxation(A,b,w,verbose,x_star)
 %[x,n_iter] = SUCCESSIVE_OVER_RELAXATION(A,b,w)
 %solves Ax=b
 %w is the relaxation parameter. w=1 is Gauss-Seidel
 %if A==A' and 0<w<2, then SOR converges
-if nargin < 4
+if ~exist('verbose','var')
   verbose = 0;
 end
+have_x_star = exist('x_star','var');
 n = size(A,1);
 assert(size(A,2)==n);
+if have_x_star, err_path = nan(1e9,1); end
 L = tril(A);
 U = A - L;
-x = L \ b;
+invL = L^-1;
+x = invL * b;
+if have_x_star, err_path(1) = norm(x-x_star); end
 tol = 1e-6;
 n_iter = 0;
 while 1
   n_iter = n_iter+1;
-  x_gs = L \ (b - U*x);
+  z = b-U*x;
+  x_gs = invL*z;
   xnew = (1-w)*x+w*x_gs;
-  if verbose && mod(n_iter,1000)==0
+  if have_x_star, err_path(1+n_iter) = norm(xnew-x_star); end
+  if verbose && mod(n_iter,10000)==0
+    disp(n_iter);
     disp(max(abs(xnew-x)));
   end
   if max(abs(xnew-x))<=tol
@@ -26,3 +33,4 @@ while 1
   end
   x = xnew;
 end
+if have_x_star, err_path = err_path(1:n_iter+1); end
